32c7f784f14009d3672e533f28888fc26308137c
ale.410 <nehringalesis@gmail.com>
2023-04-05 21:41:15 -0300

    se comenta script

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/* llamada de formularios */
+/* llamada de formulario calculadora */
 
 const formCalculadora = document.querySelector("#formCalculadora")
 
@@ -8,12 +8,21 @@ const contenedorHTML = document.querySelector(".contenedorResultado")
 
 formCalculadora.addEventListener("submit", (event) =>{
     event.preventDefault()
+
+    /* se toman datos desde formulario calculadora */
+
     const base = formCalculadora.largo.value
     const altura = formCalculadora.altura.value
     const manos = formCalculadora.manos.value
+
+    /* se establecen constantes para realizar proceso */
+
     const m2 = base * altura
     const rendimiento = 10
     const resultado = ( m2 / rendimiento ) * manos
+
+    /* se imprime resultado */
+
     contenedorHTML.innerHTML =`
     <div class="resultadojs">
         <h3><font color="black">La cantidad de litros a utilizar es: ${resultado} Litros(Rendimiento por litro 10 m2)   </h3>
@@ -29,7 +38,11 @@ const formContacto = document.querySelector("#contacto")
 
 const contenedorUsuariosHTML = document.querySelector(".contenedorUsuarios")
 
+/* se crea array de usuarios vacio */
+
 const usuarios = []
+
+/* se crea funcion para llenar el array */
 
 const renderizarUsuarios = () =>{
     contenedorUsuariosHTML.innerHTML = ""
@@ -50,6 +63,9 @@ const renderizarUsuarios = () =>{
 
 formContacto.addEventListener("submit",(event)=>{
     event.preventDefault()
+
+    /* se toman datos de formulario */
+    
     usuarios.push({
         nombre: formContacto.nombre.value,
         apellido: formContacto.apellido.value,
