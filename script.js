c6130ce00fc5d820cd72ee86a8a3152d335a4376
toto.lopez <alfonsolopezqa@gmail.com>
2023-04-08 00:16:59 -0300

    falta colores en las cajas - intente carrusel

--- script.js
+++ script.js
@@ -1,3 +1,69 @@
+/* carrusel */
+const prevBtn = document.querySelector("div.anterior");
+const nextBtn = document.querySelector("div.siguiente");
+const sectionContainer = document.querySelector("div.carousel-sections");
+
+prevBtn.onclick = anterior;
+nextBtn.onclick = siguiente;
+
+let currentIndex = 0;
+let slides = [];
+let dots = [];
+
+function render() {
+  let offset = 0;
+  slides.forEach((slide, index) => {
+    if (index < currentIndex) {
+      offset += slide.offsetWidth;
+    }
+  });
+
+  sectionContainer.style.transform = `translateX(-${offset}px)`;
+  dots.forEach((dot, index) => {
+    index === currentIndex
+      ? dot.classList.add("active")
+      : dot.classList.remove("active");
+  });
+}
+
+function anterior() {
+  if (currentIndex < 0) return;
+  currentIndex -= 1;
+  render();
+}
+
+function siguiente() {
+  if (currentIndex === slides.length - 1) return;
+  currentIndex += 1;
+  render();
+}
+
+function goto(newIndex) {
+  if (newIndex < 0 || newIndex > slides.length - 1) return;
+  currentIndex = newIndex;
+  render();
+}
+
+function init() {
+  const newSlides = document.querySelectorAll("div.carrusel-seccion > div");
+  slides = newSlides;
+
+  const newDots = document.querySelectorAll("div.carousel-dots > div");
+  newDots.forEach((dot, index) => {
+    dot.onclick = () => goto(index);
+  });
+  dots = newDots;
+
+  render();
+}
+
+init();
+
+
+
+
+
+
 /* llamada de formulario calculadora */
 
 const formCalculadora = document.querySelector("#formCalculadora")
